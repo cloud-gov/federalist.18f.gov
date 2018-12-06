d4d1ea01796dbcebfbfc9f2f17b653b02b8c0f2b
Eddie A Tejeda <eddie.tejeda@gsa.gov>
2018-12-06 13:08:55 -0500

    updatel links and update urls to base app  url

--- assets/js/index.js
+++ assets/js/index.js
@@ -93,9 +93,9 @@ switch (window.location.hostname){
     // do nothing. we keep as the first match so it's fastest in production
   break;
   case 'federalist-staging.18f.gov':
-    document.querySelector('.usa-nav-secondary-links li:last-child a').href = 'https://federalist-staging.18f.gov/auth/github';
+    document.querySelector('.usa-nav-secondary-links li:last-child a').href = 'https://federalist-staging.18f.gov';
   break;
   default:
     // this will catch different local addresses: localhost, 0.0.0.0, 127.0.0.1, etc
-    document.querySelector('.usa-nav-secondary-links li:last-child a').href = 'http://localhost:1337/auth/github';
+    document.querySelector('.usa-nav-secondary-links li:last-child a').href = 'http://localhost:1337';
 }
