ca3cf98e0cb5fe17eb3ead0adedb1acfca1343d5
Eddie A Tejeda <eddie.tejeda@gmail.com>
2019-04-08 20:18:59 -0400

    remove new for test

--- spec/basic_spec.rb
+++ spec/basic_spec.rb
@@ -4,7 +4,7 @@ describe "home page", type: :feature, js: false do
   end
   
   it "has splash message" do
-    expect(page).to have_text("There’s no new easier way to launch, maintain, and manage government sites.")    
+    expect(page).to have_text("There’s no easier way to launch, maintain, and manage government sites.")    
   end
   
   it "should have the images" do
